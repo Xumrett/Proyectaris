ac50fdeaa5b5e84f13f272152e5c35b2fbd897ab
Xumrett <103074202+Xumrett@users.noreply.github.com>
2023-01-08 17:52:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,89 @@
+import os
+import sys
+import requests
+from os import remove
+from os import  path
+from  colorama import init,Fore
+#Home programa Este Programa Ahra Casi Todo Por mi Desde Guardar Un Docunento A Combiar Renombrar Y mas vercion 1.1.3 
+
+#Recurse Home
+title_descript = "Proyecto Aris"
+print(Fore.YELLOW + title_descript.center(41,"="))
+help = " 'cd'Ver Lisga '"
+
+#recurse Sistexis function basic
+Subir = "~v"
+aceptar = "~a"
+cancelar = "~c"
+enviar = "~set"
+copiar = "~copy"
+renombrar = "~newname"
+escribir = "~while"
+cortar = "~cut"
+create = "Created"
+file_arch = "-Archive"
+file_run = "a-"
+file_run += create + file_arch;
+Archive = "Archive"
+mostrar_Carpt = "ls"
+ver_list = "cd"
+name = "Name"
+Archive += name
+eliminar = "-delete"
+eliminar_Archivos = "Archive"
+eliminar_Archivos += eliminar
+url = "home/storage/emulated/0/Download/file.txt"
+#recurse open-file python 
+db = "W"
+db_t = ".txt"
+db_csv = ".csv"
+rutate = "/storage/emulated/0/proyecto Aris/"
+rutate += db_t
+
+#recurse movilidad
+def movility(seach):
+	if seach == eliminar_Archivos:
+		 date = input('Rute/o name/:')
+		 path.exists(date)
+		 remove(date)
+	else: 
+	     print('none')
+	
+	
+	
+	return seach()
+
+#recurse Open file 
+def Ope(rutate):
+	 rutate = __file__
+	 rutate_formate = ".txt"
+	 c =  input('nombre de Archivo:/MiArchivo')
+	 c += rutate + c + rutate_formate
+	 e = input('Formato De Archivo:/txt/cvs/json')
+	 t = input('Titulo Archivo')
+	 f = input('Escribe los Datos ')
+	 
+	 
+	
+	
+	
+#recurse main bucle	
+while True:
+	seach = input('')
+	if seach == eliminar_Archivos:
+		movility(seach)
+	if seach == ver_list:
+		print('Carpeta Abtual',__file__)
+	if seach == file_run:
+		Ope(rutate)
+	if seach == 'exit':
+		sys.exit()
+	if seach == 'help':
+		print(help)
+	else: print('Comando No Valido ')
+	
+
+	
+
+
+
